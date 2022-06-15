b4e1f4ce53be33e5ebfd60f0433c88ad2f220854
Samaksh-Rawat <97016925+Samaksh-Rawat@users.noreply.github.com>
2022-06-15 20:08:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot()
     
 }
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lWUQISk0q/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KWmVAujPN/model.json',modelLoaded);
 
 function modelLoaded()
 {
@@ -30,4 +30,49 @@ function speak()
     speak_data2="The second prediction is " + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
     synth.speak(utterThis);
+}
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+ function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label=="Happy")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128522;";
+        }
+        if(results[0].label=="Sad")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128532;";
+        }
+        if(results[0].label=="Angry")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128548;";
+        }
+        if(results[1].label=="Happy")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128522;";
+        }
+        if(results[1].label=="Sad")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128532;";
+        }
+        if(results[1].label=="Angry")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128548;";
+        }
+    }
 }
